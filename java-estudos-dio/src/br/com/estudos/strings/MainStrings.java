1d6edc318a288eeefea486a22e995beba789d29a
Enzo Rossi <enzorossifs@gmail.com>
2026-02-07 12:09:03 -0300

    String, StringBuilder e StringBuffer

--- java-estudos-dio/src/br/com/estudos/strings/MainStrings.java
+++ java-estudos-dio/src/br/com/estudos/strings/MainStrings.java
@@ -2,6 +2,37 @@ package br.com.estudos.strings;
 
 public class MainStrings {
     public static void main(String[] args) {
+        /*
+        String representa texto no java, é um objeto, nao é primitivo, é imutavel
+            Comparar String com .equals()
+        */
+        //Metodos importantes de String:
+        String texto = " Java é poderoso ";
 
+        texto.length();
+        texto.trim();
+        texto.toUpperCase();
+        texto.toLowerCase();
+        texto.contains("Java");
+        texto.replace("Java", "Kotlin");
+        texto.substring(0, 4);
+
+        /*
+        StringBuilder é uma String que pode mudar, ou seja, mutavel
+            Usar quando: o texto muda muito, em loops, montagem de mensagem, performance importa
+        */
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Java");
+        sb.append(" é");
+        sb.append(" rápido");
+
+        System.out.println(sb.toString());
+
+        /*
+        StringBuffer é igual ao StringBuilder mas tem thread-safe e é mais lento
+        Quando usar: Multithreads e segurança > performance
+        */
+        StringBuffer sbf = new StringBuffer();
     }
 }
