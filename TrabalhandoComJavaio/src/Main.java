7b423434180f26ac251c033a8fd5d274987b0993
Enzo Rossi <enzorossifs@gmail.com>
2026-02-09 15:09:04 -0300

    trabahando com java.nio

--- TrabalhandoComJavaio/src/Main.java
+++ TrabalhandoComJavaio/src/Main.java
@@ -1,35 +1,35 @@
 import br.com.dio.persistence.FilePersistence;
 import br.com.dio.persistence.IOFilePersistence;
+import br.com.dio.persistence.NIOFilePersistence;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        FilePersistence persistence = new IOFilePersistence("user.csv");
-        System.out.println("========================");
-        System.out.println(persistence.write("Lucas;lucas@lucas.com;09/11/2008"));
-        System.out.println("========================");
-        System.out.println(persistence.write("Maria;maria@maria.com;11/01/2007"));
-        System.out.println("========================");
-        System.out.println(persistence.write("Leo;leo@leo.com;23/07/2006"));
-        System.out.println("========================");
+        FilePersistence persistence = new NIOFilePersistence("user.csv");
+        System.out.println(persistence.write("Bruno;bruno@bruno.com;22/09/1997;"));
+        System.out.println("======================");
+        System.out.println(persistence.write("Bernardo;bernardo@bernardo.com;28/11/1999;"));
+        System.out.println("======================");
+        System.out.println(persistence.write("Ricardo;ricardo@ricardo.com;12/01/2000;"));
+        System.out.println("======================");
         System.out.println(persistence.findAll());
-        System.out.println("========================");
-        System.out.println(persistence.remove("/01/20"));
-        System.out.println("========================");
-        System.out.println(persistence.remove("/06/202"));
-        System.out.println("========================");
-        System.out.println(persistence.findBy("Lucas;"));
-        System.out.println("========================");
-        System.out.println(persistence.findBy(";maria@"));
-        System.out.println("========================");
-        System.out.println(persistence.findBy("06"));
-        System.out.println("========================");
-        System.out.println(persistence.replace(".com;09/11/", "Carlos;carlos@carlos.com;22/12/2008"));
-        System.out.println("========================");
-        System.out.println(persistence.findAll());
-        System.out.println("========================");
+        System.out.println("======================");
+        System.out.println(persistence.remove(";ricardo@"));
+        System.out.println("======================");
+        System.out.println(persistence.remove(";joana@"));
+        System.out.println("======================");
+        System.out.println(persistence.findBy(";bruno@"));
+        System.out.println("======================");
+        System.out.println(persistence.findBy(";ricardo@"));
+        System.out.println("======================");
+        System.out.println(persistence.replace("Bernardo", "Lucas;lucas@lucas.com;17/12/1988"));
+        System.out.println("======================");
+        System.out.println(persistence.replace("Joaquim", "Lucas;lucas@lucas.com;17/12/1988"));
+        System.out.println("======================");
+
+
 
     }
 }
